142c8a6ffe62db4c9f457abe195d0baf5885564a
Collin Shriner <collinshriner9@gmail.com>
2023-06-13 16:46:27 -0400

    using primary to specify what bean to inject if no qualifier is present

--- src/main/java/shriner/springframework/sfgdi/controllers/MyController.java
+++ src/main/java/shriner/springframework/sfgdi/controllers/MyController.java
@@ -1,13 +1,18 @@
 package shriner.springframework.sfgdi.controllers;
 
 import org.springframework.stereotype.Controller;
+import shriner.springframework.sfgdi.services.GreetingService;
 
 @Controller
 public class MyController {
 
-    public String sayHello(){
-        System.out.println("Hello World!!!");
+    private final GreetingService greetingService;
 
-        return "Hi Folks!";
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String sayHello(){
+        return greetingService.sayGreeting();
     }
 }
